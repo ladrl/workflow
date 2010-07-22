442bc3b1c14cce2a8dcc21969895e5ee6ff300ff
Lukas Lädrach <lukasladrach@Lukas-Ladrachs-MacBook-Pro-2.local>
2010-07-22 17:38:23 +0200

    Workflow, not really working. The problem seems to be the unclear domain model and the corresponding terminology-confusion

--- src/main/scala/clamp/gate/GateWorkflow.scala
+++ src/main/scala/clamp/gate/GateWorkflow.scala
@@ -13,10 +13,12 @@ Usage:
  */
 trait WorkflowMessage
 
+class WorkflowException(msg: String) extends Exception(msg)
+
 // Trait for a single chunk of work. Maintains a sequence of following items, each associated with a conditon function
 // 
 trait WorkItem{
-	val followingItems: WorkItem.ItemsList
+	val followingItems: List[WorkItem.Item]
 	val messagePreProcessor: Option[WorkItem.MessageProcessor]
 	val action: (WorkflowMessage) => Unit
 	def process(msg: WorkflowMessage, state: WorkflowState): WorkflowState = {
@@ -26,11 +28,15 @@ trait WorkItem{
 				
 				action(preProcessedMsg) // Execute assigned action with preprocessed message and no checks
 				
-				followingItems.dropWhile(_ match {
-					case (Some(check), _) => check(state, preProcessedMsg)
-					case (None, _) => true
-				}) match {
-					case (_, nextItem) :: _ => new WorkflowInstance(nextItem)
+				def executeCheck(listEntry: (Option[WorkItem.Condition], Option[WorkItem])): Boolean = 
+					listEntry._1 match {
+						case Some(check) => check(state, preProcessedMsg)
+						case None => true
+					}
+				
+				followingItems.dropWhile(executeCheck(_) == false) match {
+					case (_, Some(nextItem)) :: _ => new WorkflowInstance(nextItem)
+					case (_, None) :: _ => throw new WorkflowException("Unfinished workflow encountered")
 					case Nil => WorkflowEnd
 				}
 			}
@@ -41,7 +47,7 @@ trait WorkItem{
 // Companion class holding the associated types
 object WorkItem {
 	type Condition = (WorkflowState, WorkflowMessage) => Boolean
-	type ItemsList = List[(Option[Condition], WorkItem)]
+	type Item = (Option[Condition], Option[WorkItem])
 	type MessageProcessor = WorkflowMessage => WorkflowMessage
 }
 
@@ -62,18 +68,30 @@ Workflow()
 	then when 
 */
 case class WorkflowItem(
-	val work: Option[(WorkflowMessage) => Unit], 
-	val followingItems: WorkItem.ItemsList, 
-	val messagePreProcessor: Option[WorkItem.MessageProcessor]) extends WorkItem {
+	val work: Option[(WorkflowMessage) => Unit] = None, 
+	val followingItems: List[WorkItem.Item] = Nil, 
+	val messagePreProcessor: Option[WorkItem.MessageProcessor] = None) extends WorkItem {
 		
 	def then(item: WorkItem) = {
-		val newFollowingItems = followingItems ++ List(None -> item)
+		val newFollowingItems:List[WorkItem.Item] = followingItems match {
+			case lastItem :: Nil => lastItem match {
+				case (_, None) => followingItems.dropRight(1) ++ List((lastItem._1, Some(item)))
+				case _ => followingItems ++ List((None, Some(item)))
+			}
+			case Nil => List((None, Some(item)))
+		}
 		copy(followingItems = newFollowingItems)
 	}
 	
 	def when(check: WorkItem.Condition) = {
-		val newFollowingItems = followingItems ++ List(check -> followingItems.last)
-		copy(followingItems = newFollowingItems.asInstanceOf[WorkItem.ItemsList])
+		val newFollowingItems:List[WorkItem.Item] = followingItems match {
+			case lastItem :: Nil => lastItem match {
+				case (None, _) => followingItems.dropRight(1) ++ List((Some(check), lastItem._2))
+				case _ => followingItems ++ List((Some(check), None))
+			}
+			case Nil => List((Some(check), None))
+		}
+		copy(followingItems = newFollowingItems)
 	}
 	
 	def preprocess(preproc: WorkItem.MessageProcessor) = copy(messagePreProcessor = Some(preproc))
--- src/test/scala/clamp/gate/GateWorkflowTest.scala
+++ src/test/scala/clamp/gate/GateWorkflowTest.scala
@@ -4,7 +4,6 @@ import org.scalatest._
 import org.scalatest.matchers._
 
 class GateWorkflowTest extends FlatSpec with MustMatchers {
-	{
 		val item = WorkflowItem(None, Nil, None)
 		"A empty workflow item" must "process a message and return the EndWorkflow state" in {
 			item process (new WorkflowMessage {}, WorkflowInstance(item)) must be (WorkflowEnd)
@@ -18,5 +17,33 @@ class GateWorkflowTest extends FlatSpec with MustMatchers {
 			itemWithWork process (new WorkflowMessage{}, WorkflowInstance(itemWithWork)) must be (WorkflowEnd)
 			workDone must be (true)
 		}
-	}
+		
+		it must "allow to define a no-condition follow up item" in {
+			((item then item) followingItems(0) _2) must be (Some(item))
+		}
+		
+		it must "allow to define a conditional follow up item" in {
+			val otherItem = WorkflowItem(None, Nil, None)
+			val condItem = item when ((_, _) => {true}) then otherItem
+			val altCondItem = WorkflowItem()
+			(condItem followingItems) must be (List((Some((_,_) => {true}), Some(otherItem))))
+//			condItem process (new WorkflowMessage{}, WorkflowInstance(item)) must be (WorkflowInstance(otherItem))
+		}
+		
+		"A workflow with multiple follow ups" must "correctly select the follow up item" in {
+			pending
+			case class Message[T](val value: T) extends WorkflowMessage
+			val otherItem1 = WorkflowItem()
+			println(otherItem1)
+			val otherItem2 = WorkflowItem()
+			val condItem = item
+				.when ((_, x) =>{ x match {case Message(i:Int) => {println("got %d" format i); true}; case _ => false}}) then 
+						otherItem1 
+				.when ((_, x) => {println("Check 2"); x match {case Message(s:String) => true; case _ => false}}) then 
+						otherItem2
+							
+			println(condItem followingItems)
+			condItem process (new Message(0), WorkflowInstance(condItem)) must be (WorkflowInstance(otherItem1))
+//			condItem process (new Message(""), WorkflowInstance(condItem)) must be (WorkflowInstance(otherItem2))
+		}
 }
